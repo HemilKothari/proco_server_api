d1df8714da329a26b8ee71aecdb7baba3add0123
Nikhil <nikhilparbat908@gmail.com>
2025-03-17 23:46:27 +0530

    User Controller Updated By Rishi Sahab

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,54 +1,97 @@
 const User = require("../models/User");
 
 module.exports = {
-    updateUser: async (req, res) => {
-        if (req.body.password) {
-            req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET).toString();
-        }
-        try {
-            const updatedUser = await User.findByIdAndUpdate(
-                req.user.id, {
-                $set: req.body
-            }, { new: true });
-            const { password, __v, createdAt, ...others } = updatedUser._doc;
+  updateUser: async (req, res) => {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.SECRET
+      ).toString();
+    }
+    try {
+      if (req.user.id) {
+        res.status(400).json("User Id is required");
+      }
+      const updatedUser = await User.findByIdAndUpdate(
+        req.user.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      if (!updatedUser) {
+        res.status(404).json("Cannot update user");
+      }
+      const { password, __v, createdAt, ...others } = updatedUser._doc;
 
-            res.status(200).json({ ...others });
-            // res.status(200).json(updatedUser);
-        } catch (err) {
-            res.status(500).json(err)
-        }
-    },
+      res.status(200).json({ ...others });
+      // res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-    deleteUser: async (req, res) => {
-        try {
-            await User.findByIdAndDelete(req.user.id)
-            res.status(200).json("Successfully Deleted")
-        } catch (error) {
-            res.status(500).json(error)
-        }
-    },
+  deleteUser: async (req, res) => {
+    try {
+      if (req.user.id) {
+        res.status(400).json("User Id is required");
+      }
+      await User.findByIdAndDelete(req.user.id);
 
-    getUser: async (req, res) => {
-        try {
-            const user = await User.findById(req.user.id);
-            const { password, __v, createdAt, ...userdata } = user._doc;
-            res.status(200).json(userdata)
-        } catch (error) {
-            res.status(500).json(error)
-        }
-    },
+      res.status(200).json("Successfully Deleted");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
 
+  getUser: async (req, res) => {
+    try {
+      if (req.user.id) {
+        res.status(400).json("User Id is required");
+      }
+      const user = await User.findById(req.user.id);
+      if (user) {
+        res.status(404).json("Cannot find user");
+      }
+      const { password, __v, createdAt, ...userdata } = user._doc;
+      if (!userdata) {
+        res.status(404).json("Cannot find user");
+      }
+      res.status(200).json(userdata);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
 
-    getAllUsers: async (req, res) => {
-        try {
-            const allUser = await User.find();
+  getAllUsers: async (req, res) => {
+    try {
+      const allUser = await User.find();
+      if (!allUser) {
+        res.status(404).json("Cannot find user");
+      }
 
-            res.status(200).json(allUser)
-        } catch (error) {
-            res.status(500).json(error)
-        }
-    },
+      res.status(200).json(allUser);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
 
-
-
-}
+  getSwipedUser: async (req, res) => {
+    try {
+      if (req.user.id) {
+        res.status(400).json("User Id is required");
+      }
+      const user = await User.findById(req.user.id);
+      if (!user) {
+        res.status(404).json("Cannot find user");
+      }
+      const { name, profile, location, skills } = user._doc;
+      if (!name || !profile || !location || !skills) {
+        res.status(404).json("Cannot find user");
+      }
+      res.status(200).json({ name, profile, location, skills });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+};
